64d7d8c836eadb459eb83b29766de5b61164dd9e
Rajadurai31 <kit27.csbs45@gmail.com>
2025-09-16 14:53:44 +0530

    Exception handling

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,6 +1,19 @@
 package com.todo;
+import com.todo.util.DatabaseConnection;
+import com.todo.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main{
     public static void main(String[] args){
-
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try{
+            Connection cn =db_Connection.getDBConnection();
+            System.out.println("Connection established");
+        }
+        catch(SQLException e){
+            System.out.println("Conection Failed");
+        }
     }
 }
