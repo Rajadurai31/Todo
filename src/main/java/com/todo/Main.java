dc46009b2885a177d811d1d2ea907b5f0f719815
Rajadurai31 <kit27.csbs45@gmail.com>
2025-09-17 14:53:56 +0530

    GUI updated

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,12 +1,14 @@
 package com.todo;
+import com.todo.gui.TodoAppGUI;
 import com.todo.util.DatabaseConnection;
 import com.todo.util.DatabaseConnection;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         DatabaseConnection db_Connection = new DatabaseConnection();
         try{
             Connection cn =db_Connection.getDBConnection();
@@ -14,6 +16,23 @@ public class Main{
         }
         catch(SQLException e){
             System.out.println("Conection Failed");
+            System.exit(1);
         }
+
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (Exception e) {
+//            System.err.println("could not set look and feel ",e.getMessage());
+//        }
+        SwingUtilities.invokeLater(
+                ()->{
+                    try {
+                        new TodoAppGUI().setVisible(true);
+                    } catch (Exception e) {
+                        System.err.println("Error strarting the application "+e.getLocalizedMessage());
+                    }
+                }
+        );
     }
 }
